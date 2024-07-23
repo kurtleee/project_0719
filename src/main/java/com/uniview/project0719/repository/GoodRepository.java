d552a2507fe4f9c5c26ef064763923e038f22b69
yuki <chouyuki00@gmail.com>
2024-07-23 16:58:35 +0800

    完成7.5更新商品； 7.6删除商品； 7.7获取订单列表；

---
+++ src/main/java/com/uniview/project0719/repository/GoodRepository.java
@@ -0,0 +1,8 @@
+package com.uniview.project0719.repository;
+
+import com.uniview.project0719.entity.Good;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GoodRepository extends JpaRepository<Good, Integer> {
+
+}
