b03c4e0f58193d3527b198436b29ba82902b842e
zhouxin <zh>
2024-07-23 23:20:57 +0800

    完成订单查询功能

--- src/main/java/com/uniview/project0719/service/UserOrderService.java
+++ src/main/java/com/uniview/project0719/service/UserOrderService.java
@@ -2,6 +2,7 @@ package com.uniview.project0719.service;
 
 import com.uniview.project0719.dto.OrderParamDto;
 import com.uniview.project0719.entity.UserOrder;
+import com.uniview.project0719.utils.ParamData;
 import com.uniview.project0719.utils.ResponseData;
 import org.springframework.data.domain.Page;
 
@@ -21,4 +22,10 @@ public interface UserOrderService {
      */
     ResponseData<?> createOrder(OrderParamDto orderParamDto) throws InterruptedException;
 
+    /**
+     * 获取客户订单列表
+     * @param paramData
+     * @return ResponseData<?>
+     */
+    ResponseData<?> getUserOrderList(ParamData<UserOrder> paramData);
 }
