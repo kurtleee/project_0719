929bbc1c63ba3da3a9db1959d32e83070164dca8
Krut Lee <59387051+kurtleee@users.noreply.github.com>
2024-07-23 11:19:01 +0800

    删除冗余文件

--- src/main/java/com/uniview/project0719/repository/UserInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.uniview.project0719.repository;
-
-import com.uniview.project0719.entity.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
-
-}
