e2469d00fe3e2e4a4f1178d7c9287ace81e78e4d
zhouxin <zh>
2024-07-24 17:43:53 +0800

    找回UserGoodService

---
+++ src/main/java/com/uniview/project0719/service/UserGoodService.java
@@ -0,0 +1,16 @@
+package com.uniview.project0719.service;
+
+import com.uniview.project0719.entity.Good;
+import com.uniview.project0719.utils.ResponseData;
+
+
+public interface UserGoodService {
+    /**
+     * 获取商品列表
+     */
+    ResponseData<?> findAllGood(Integer classificationId, Integer page, Integer size);
+    /**
+     * 获取商品详情
+     */
+    ResponseData<?> findGoodById(Integer id);
+}
