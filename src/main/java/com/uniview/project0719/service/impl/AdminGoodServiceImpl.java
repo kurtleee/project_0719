9fa42ae40f0478e2e33afb8a8d86cd8c3088088f
zhouxin <zh>
2024-07-24 11:50:28 +0800

    添加AdminService类、修改Specification方法符合命名规范、给GoodService重命名以便与Admin区分

---
+++ src/main/java/com/uniview/project0719/service/impl/AdminGoodServiceImpl.java
@@ -0,0 +1,13 @@
+package com.uniview.project0719.service.impl;
+
+import com.uniview.project0719.service.AdminGoodService;
+
+/**
+ * @author ：zx
+ * @date ：Created in 2024/7/24 11:39
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class AdminGoodServiceImpl implements AdminGoodService {
+}
