c34906b98e2269cc5663f02296012788678f114a
zhouxin <zh>
2024-07-26 12:00:14 +0800

    添加swagger-ui

--- src/main/java/com/uniview/project0719/controller/RepositoryController.java
+++ src/main/java/com/uniview/project0719/controller/RepositoryController.java
@@ -4,6 +4,8 @@ import com.uniview.project0719.dto.RepositoryDTO;
 import com.uniview.project0719.service.RepositoryService;
 import com.uniview.project0719.utils.ParamData;
 import com.uniview.project0719.utils.ResponseData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,15 +18,18 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/repository")
+@Tag(name = "仓库接口")
 public class RepositoryController {
     @Autowired
     private RepositoryService repositoryService;
 
     @PostMapping("/getRepositoryList")
+    @Operation(summary = "获取仓库列表")
     public ResponseData<?> getRepositoryList(@RequestBody ParamData<RepositoryDTO> paramData) {
         return repositoryService.findRepositories(paramData);
     }
     @GetMapping("/getRepositoryDetail/{repositoryId}")
+    @Operation(summary = "获取仓库详情")
     public ResponseData<?> getRepositoryDetail(@PathVariable("repositoryId") Integer id){
         return repositoryService.findRepositoryDetail(id);
     }
