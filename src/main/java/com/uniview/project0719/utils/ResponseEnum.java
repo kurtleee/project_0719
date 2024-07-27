a8270d65c8bbb76383d53679f2e16fb82cb1c7a5
Krut Lee <59387051+kurtleee@users.noreply.github.com>
2024-07-27 15:07:43 +0800

    重构优惠券控制器使用统一返回类；增加ResponseEnum枚举相关错误操作码

--- src/main/java/com/uniview/project0719/utils/ResponseEnum.java
+++ src/main/java/com/uniview/project0719/utils/ResponseEnum.java
@@ -10,7 +10,9 @@ public enum ResponseEnum {
     FAIL(400,"请求参数错误"),
     NO_AUTHORITY(401,"未授权"),
     NOT_FOUND(404,"资源未找到"),
-    INTERVAL_ERROR(500,"服务器内部错误");
+    INTERVAL_ERROR(500,"服务器内部错误"),
+    COUPON_NOT_AVAILABLE(801,"优惠券不可用"),
+    COUPON_NOT_FOUND(802,"优惠券不存在");
     private final Integer code;
     private final String msg;
 }
