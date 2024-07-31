661e97ca8b025b9f9b222aaba852bd47c00a5174
Krut Lee <59387051+kurtleee@users.noreply.github.com>
2024-07-31 11:17:25 +0800

    增强JWT工具泛型安全；添加SECRET_KEY注释；在 MACSigner 构造函数中使用 UTF-8 编码将密钥转换为字节数组。

--- src/main/java/com/uniview/project0719/utils/JWTUtil.java
+++ src/main/java/com/uniview/project0719/utils/JWTUtil.java
@@ -3,6 +3,7 @@ package com.uniview.project0719.utils;
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.MACSigner;
 
+import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.util.Map;
 /**
@@ -10,16 +11,19 @@ import java.util.Map;
  */
 
 public class JWTUtil {
-    public static String createJWT(Map userMap) throws JOSEException {
+    public static String createJWT(Map<String, Object> userMap) throws JOSEException {
         JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.HS256).type(JOSEObjectType.JWT).build();
         Payload payload = new Payload(userMap);
         JWSObject jwsObject = new JWSObject(header, payload);
-        String secretKey = "中中中中中中中中中中中";
-        MACSigner macSigner = new MACSigner(secretKey);
+        // SECRET_KEY 为 32 字节长度的字符串，以符合 HS256 算法的要求。这里暂不做修改
+        // 定义常量使用命名规则，全大写，单词之间用下划线分隔
+        String SECRET_KEY = "中中中中中中中中中中中";
+        // 在 MACSigner 构造函数中使用 UTF-8 编码将密钥转换为字节数组。
+        MACSigner macSigner = new MACSigner(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
         jwsObject.sign(macSigner);
         return jwsObject.serialize();
     }
-    public static Map getJWTUserInfo(String jwt) throws ParseException {
+    public static Map<String, Object> getJWTUserInfo(String jwt) throws ParseException {
         JWSObject parse = JWSObject.parse(jwt);
         return parse.getPayload().toJSONObject();
 
